47fb03112c946e46fa05a08095497b6154189e1f
Sidhanti Patil <sidpatil2209@gmail.com>
2022-02-22 19:46:32 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/TokenInfo/Header';
+import GetBalance from './components/TokenInfo/GetBalance';
+import BuyForm from './components/PersonalPanel/BuyForm';
+import TransferForm from './components/PersonalPanel/TransferForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     <GetBalance/>
+     <div>Wanna transfer?</div>
+     <TransferForm/>
+     <div>Wanna buy?</div>
+     <BuyForm/>
+    </div>
+  );
+}
+
+export default App;
